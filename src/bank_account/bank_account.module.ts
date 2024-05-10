505a460915ff05b055bbbe922a997edf52a4a0b3
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-05-11 06:07:22 +0700

    add CRUD  bank account, variant option value & stores

--- src/bank_account/bank_account.module.ts
+++ src/bank_account/bank_account.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BankAccountService } from './bank_account.service';
 import { BankAccountController } from './bank_account.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [BankAccountController],
-  providers: [BankAccountService],
+  providers: [BankAccountService, PrismaService],
 })
 export class BankAccountModule {}
